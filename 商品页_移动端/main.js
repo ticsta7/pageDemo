bc831d54226e3f2b149467690f9ed057d007edcf
ticsta7 <ticsta7@163.com>
2020-01-09 22:02:39 +0800

    ？？

--- 商品页_移动端/main.js
+++ 商品页_移动端/main.js
@@ -1,14 +1,29 @@
-// $(function(){
-//     $(".productTitle").on("swipe",function(){
-//         alert("滑动!");
-//     });
-// }); 
+$(function(){
 
+    var test = 1;
 
-$(document).on("pagecreate","#pageone",function(){
-       
-    $(".productTitle").on("swipe",function(){
-        alert("滑动");
+    var imgOrder = $(".imgOrder span").text();
+
+    $(".recommend .right").on("click",function(){
+        $(".productImg ul").animate({left:"+=-600px"});
       }); 
 
-  });
+      $(".recommend .left").on("click",function(){
+        $(".productImg ul").animate({left:"+=600px"});
+      }); 
+      
+}); 
+
+
+// $(document).on("pageload","#pageone",function(){
+    
+//     // var imgOrder = $(".imgOrder span").text();
+
+//     $(".recommend .right").on("click",function(){
+//         $(".productImg ul").animate({left:"+=-600px"});
+//       }); 
+
+//       $(".recommend .left").on("click",function(){
+//         $(".productImg ul").animate({left:"+=600px"});
+//       }); 
+//   });
