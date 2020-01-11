2ce538eac73437d1dbe7d0e9b1abf0fd81e515d2
ticsta7 <ticsta7@163.com>
2020-01-11 21:27:37 +0800

    +

--- 商品页_移动端/main.js
+++ 商品页_移动端/main.js
@@ -1,46 +1,28 @@
-$(function(){
-    var imgOrder = $(".imgOrder span").text();
-    var width =$(".productImg").width();
-    console.log(width);
-
-    
-
-    $(".recommend .right").on("click",function(){
-        if(imgOrder < 5){
-            imgOrder++;
-            $(".productImg ul").animate({'left':"+=-600px"});
-        }
-        else{
-            imgOrder=1;
-            $(".productImg ul").animate({left:"+=2400px"});
-        }
-        $(".imgOrder span").text(imgOrder);
-      }); 
-
-
-
-      $(".recommend .left").on("click",function(){
-        if(imgOrder != 1){
-        imgOrder--;
-        $(".productImg ul").animate({left:"+=600px"});
-    }
-
-        $(".imgOrder span").text(imgOrder);
-      }); 
-      
-}); 
-
-
-
-// $(document).on("pageload","#pageone",function(){
-    
-//     // var imgOrder = $(".imgOrder span").text();
+// $(function(){
+//     var imgOrder = $(".imgOrder span").text();
+//     var width =$(".container").width();
+//     console.log(width);
 
 //     $(".recommend .right").on("click",function(){
-//         $(".productImg ul").animate({left:"+=-600px"});
+//         if(imgOrder < 5){
+//             imgOrder++;
+//             $(".productImg ul").animate({'left':"+=-600px"});
+//         }
+//         else{
+//             imgOrder=1;
+//             $(".productImg ul").animate({left:"+=2400px"});
+//         }
+//         $(".imgOrder span").text(imgOrder);
 //       }); 
 
-//       $(".recommend .left").on("click",function(){
+//     $(".recommend .left").on("click",function(){
+//         if(imgOrder != 1){
+//         imgOrder--;
 //         $(".productImg ul").animate({left:"+=600px"});
+//     }
+//         $(".imgOrder span").text(imgOrder);
 //       }); 
-//   });
+      
+// }); 
+
+var aimg = document.querySelector('.productImg');
